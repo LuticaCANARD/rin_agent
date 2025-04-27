4605891c952952540dd68f0d81a0ba2a7afc2536
Lutica_CANARD <presan100@gmail.com>
2025-04-27 23:25:55 +0900

    Cargo.toml 및 Cargo.lock에 reqwest 및 serde_json 추가, docker-compose.yml 파일 생성, GeminiClient 구조체 수정 및 비동기 요청 기능 구현

--- src/gemini/gemini_client.rs
+++ src/gemini/gemini_client.rs
@@ -1,56 +1,62 @@
-pub mod gemini_client{
-    use std::io::{stdout, Write};
-    use curl::easy::Easy;
-    use std::env;
-    use std::sync::{Arc, Mutex};
+use std::env;
+use reqwest::Client;
+use serde_json::json;
 
-    struct GeminiClient {
-        net_client: Easy,
-    }
+pub struct GeminiClient {
+    net_client: Client,
+}
 
-    trait GeminiClientTrait {
-        fn new() -> Self;
-        fn send_request(&mut self, url: &str) -> Result<String, String>;
-        
-    }
-    impl GeminiClientTrait for GeminiClient {
-        fn new() -> Self {
-            GeminiClient {
-                net_client: Easy::new(),
-            }
+trait GeminiClientTrait {
+    fn new() -> Self;
+    async fn send_query_to_gemini(&mut self, query: Vec<&str>) -> Result<String, String>;
+    
+}
+impl GeminiClientTrait for GeminiClient {
+    fn new() -> Self {
+        GeminiClient {
+            net_client: Client::new()
         }
+    }
 /**
-     * curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}" \
-    -H 'Content-Type: application/json' \
-    -X POST \
-    -d '{
-        "contents": [
+ * curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}" \
+-H 'Content-Type: application/json' \
+-X POST \
+-d '{
+    "contents": [
+    {
+        "parts": [
         {
-            "parts": [
-            {
-                "text": "Write a story about a magic backpack."
-            }
-            ]
+            "text": "Write a story about a magic backpack."
         }
         ]
-    }'
- * 
- */
-        fn send_request(&mut self, url: &str) -> Result<String, String> {
-            let data = Arc::new(Mutex::new(Vec::new()));
-            self.net_client.url(url).unwrap();
-            let mut transfer = self.net_client.transfer();
-            {
-                let data = Arc::clone(&data);
-                transfer.write_function(move |new_data| {
-                    let mut data = data.lock().unwrap();
-                    data.extend_from_slice(new_data);
-                    Ok(new_data.len())
-                }).unwrap();
-            }
-            transfer.perform().unwrap();
-            let response = String::from_utf8(data.lock().unwrap().clone()).unwrap();
-            Ok(response)
-        }
-    } 
-}
+    }
+    ]
+}'
+* 
+*/
+    async fn send_query_to_gemini(&mut self, query: Vec<&str>) -> Result<String, String> {
+        let api_key = env::var("GEMINI_API_KEY").expect("GEMINI_API_KEY must be set");
+        let url = format!("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={}", api_key);
+        let objected_query = json!({
+            "contents": [
+                {
+                    "parts": query.iter().map(|&q| {
+                        json!({ "text": q })
+                    }).collect::<Vec<_>>()
+                }
+            ]
+        });
+        
+        let response = self.net_client
+            .post(&url)
+            .header("Content-Type", "application/json")
+            .body(objected_query.to_string())
+            .send()
+            .await
+            .map_err(|e| e.to_string())?;
+        
+        let response_str = response.text().await.map_err(|e| e.to_string())?;
+
+        Ok(response_str)
+    }
+} 
