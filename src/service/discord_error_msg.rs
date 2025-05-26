4864a5dc6f3aa6a7e0f16f9d434be945cd22d68a
Lutica_CANARD <presan100@gmail.com>
2025-05-26 23:55:34 +0900

    기능 개선: 디스코드 로그 전송 시 환경에 따라 'dev' 또는 'prod' 정보를 포함하도록 수정

--- src/service/discord_error_msg.rs
+++ src/service/discord_error_msg.rs
@@ -7,7 +7,11 @@ pub async fn send_debug_error_log(
     message: String
 ){
     dotenv::dotenv().ok();
-
+    let is_dev = if cfg!(debug_assertions) {
+        "dev"
+    } else {
+        "prod"
+    };
   let token = std::env::var("DISCORD_WEBHOOK_TOKEN").unwrap_or_else(|_| "default_token".to_string());
   reqwest::Client::new()
     .post(format!("https://discord.com/api/webhooks/1375871801409798186/{}", token))
@@ -17,7 +21,7 @@ pub async fn send_debug_error_log(
             "embeds": [
                 {
                     "title": "Rin Agent Debug Error",
-                    "description": message,
+                    "description": format!("{}> {}",is_dev,message),
                     "color": 16711680, // Red color
                     "footer": {
                         "text": "Rin Agent Debug Log"
@@ -39,6 +43,11 @@ pub async fn send_additional_log(
 ){
     dotenv::dotenv().ok();
     let token = std::env::var("DISCORD_WEBHOOK_TOKEN").unwrap_or_else(|_| "default_token".to_string());
+    let is_dev = if cfg!(debug_assertions) {
+        "dev"
+    } else {
+        "prod"
+    };
     let req = reqwest::Client::new()
         .post(format!("https://discord.com/api/webhooks/1375871801409798186/{}", token))
         .header("Content-Type", "application/json")
@@ -47,7 +56,7 @@ pub async fn send_additional_log(
                 "embeds": [
                     {
                         "title": "Rin Agent Additional Log",
-                        "description": message,
+                        "description": format!("{}> {}",is_dev,message),
                         "color": 16776960, // Yellow color
                         "footer": {
                             "text": "Rin Agent Additional Log"
