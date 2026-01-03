63d1e5bc08414e96e5db341401a9d6634c5e6033
Lutica_CANARD <presan100@gmail.com>
2026-01-04 00:36:15 +0900

    공백 사이즈

--- manager/src/libs/find_server.rs
+++ manager/src/libs/find_server.rs
@@ -1,14 +1,14 @@
 use sysinfo::{Pid, System};
 
 pub fn get_server_pid(process_name: &str) -> Option<Pid> {
-    let mut sys = System::new_all();
-    sys.refresh_all();
-
-    for (pid, process) in sys.processes() {
-        if *process.name().to_ascii_lowercase() == *process_name.to_ascii_lowercase()
-          {
-            return Some(*pid);
-        }
+  let mut sys = System::new_all();
+  sys.refresh_all();
+  
+  for (pid, process) in sys.processes() {
+    if *process.name().to_ascii_lowercase() == *process_name.to_ascii_lowercase()
+    {
+      return Some(*pid);
     }
-    None
+  }
+  None
 }
--- rin_agent/gemini_live_api/src/libs/logger.rs
+++ rin_agent/gemini_live_api/src/libs/logger.rs
@@ -1,44 +1,44 @@
 use lazy_static::lazy_static;
 #[derive(PartialEq, Eq)]
 pub enum LogLevel {
-    Error,
-    Warning,
-    Info,
-    Debug,
+  Error,
+  Warning,
+  Info,
+  Debug,
 }
 static LOG_LEVEL_TO_STRING: [&str; 4] = ["Error", "Warning", "Info", "Debug"];
 impl LogLevel {
-    pub fn to_string(&self) -> &str {
-        match self {
-            LogLevel::Error => LOG_LEVEL_TO_STRING[0],
-            LogLevel::Warning => LOG_LEVEL_TO_STRING[1],
-            LogLevel::Info => LOG_LEVEL_TO_STRING[2],
-            LogLevel::Debug => LOG_LEVEL_TO_STRING[3],
-        }
+  pub fn to_string(&self) -> &str {
+    match self {
+      LogLevel::Error => LOG_LEVEL_TO_STRING[0],
+      LogLevel::Warning => LOG_LEVEL_TO_STRING[1],
+      LogLevel::Info => LOG_LEVEL_TO_STRING[2],
+      LogLevel::Debug => LOG_LEVEL_TO_STRING[3],
     }
+  }
 }
 pub struct Logger {
-    log_fn: fn(&str,LogLevel),
+  log_fn: fn(&str,LogLevel),
 }
 
 impl Logger {
-    pub fn new(log_fn: fn(&str,LogLevel)) -> Self {
-        Logger { log_fn }
+  pub fn new(log_fn: fn(&str,LogLevel)) -> Self {
+    Logger { log_fn }
+  }
+  
+  pub fn log(&self, level: LogLevel, message: &str) {
+    match level {
+      LogLevel::Error => (self.log_fn)(message, LogLevel::Error),
+      LogLevel::Warning => (self.log_fn)(message, LogLevel::Warning),
+      LogLevel::Info => (self.log_fn)(message, LogLevel::Info),
+      LogLevel::Debug => if cfg!(debug_assertions) {
+        (self.log_fn)(message, LogLevel::Debug)
+      },
     }
-
-    pub fn log(&self, level: LogLevel, message: &str) {
-        match level {
-            LogLevel::Error => (self.log_fn)(message, LogLevel::Error),
-            LogLevel::Warning => (self.log_fn)(message, LogLevel::Warning),
-            LogLevel::Info => (self.log_fn)(message, LogLevel::Info),
-            LogLevel::Debug => if cfg!(debug_assertions) {
-                (self.log_fn)(message, LogLevel::Debug)
-            },
-        }
-    }
+  }
 }
 lazy_static! {
-    pub static ref LOGGER: Logger = Logger::new(|message, level| {
-        println!("[{:?}] {}", level.to_string(), message);
-    });
+  pub static ref LOGGER: Logger = Logger::new(|message, level| {
+    println!("[{:?}] {}", level.to_string(), message);
+  });
 }
